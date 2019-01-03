aa87e17a3b2859122568f0813bf9ecdf1b408f7b
Clemens Radermacher <clemera@posteo.net>
2019-01-03 12:54:25 +0100

    Verify download

--- build-emacs.sh
+++ build-emacs.sh
@@ -8,6 +8,26 @@ set -eu
 
 readonly version="26.1"
 
+# download source package
+if [[ ! -d emacs-"$version" ]]; then
+
+   wget https://ftp.gnu.org/gnu/gnu-keyring.gpg
+   wget https://ftp.gnu.org/gnu/emacs/emacs-"$version".tar.xz
+   wget https://ftp.gnu.org/gnu/emacs/emacs-"$version".tar.xz.sig
+
+   gpg --verify --no-default-keyring --keyring ./gnu-keyring.gpg emacs-"$version".tar.xz.sig
+
+   read -p "Continue (y/n)?" choice
+   case "$choice" in
+       y|Y ) : ;;
+       * ) exit 1;;
+   esac
+
+   tar xvf emacs-"$version".tar.xz
+
+fi
+
+
 # install dependencies
 sudo apt-get -qq update
 sudo apt-get -qq install -y stow build-essential libx11-dev \
@@ -17,30 +37,23 @@ sudo apt-get -qq install -y stow build-essential libx11-dev \
      libgtk-3-dev libwebkitgtk-3.0-dev libxpm-dev libjansson-dev wget
 
 # from Ubuntu 16.10, libgnutls-dev, libpng12-dev is no longer available
-sudo apt-get -qq install libgnutls-dev libpng12-dev || \
-    sudo apt-get -qq install libgnutls28-dev libpng-dev
-
-# download source package
-if [[ ! -d emacs-"$version" ]]; then
-   wget http://ftp.gnu.org/gnu/emacs/emacs-"$version".tar.xz
-   tar xvf emacs-"$version".tar.xz
-fi
+sudo apt-get -qq install libgnutls28-dev libpng-dev
 
 # create /usr/local subdirectories
 sudo mkdir -p /usr/local/{bin,etc,games,include,lib,libexec,man,sbin,share,src}
 
 # build and install
 sudo mkdir -p /usr/local/stow
+sudo chown -R clemera /usr/local/stow
+
 cd emacs-"$version"
 ./configure \
     --with-xft \
-    --with-x-toolkit=gtk3
+    --with-x-toolkit=gtk3\
+    --with-modules
 
-make
-sudo make \
-    install-arch-dep \
-    install-arch-indep \
-    prefix=/usr/local/stow/emacs-"$version"
+# make
+make install prefix=/usr/local/stow/emacs-"$version"
 
 cd /usr/local/stow
 sudo stow emacs-"$version"
