a6db073956ee1c5a55288826dec6110cca327a9f
Clemens Radermacher <clemera@posteo.net>
2020-07-18 23:24:56 +0200

    Update native.sh

--- native.sh
+++ native.sh
@@ -2,3 +2,4 @@
 git checkout feature/native-comp
 ./autogen.sh
 ./configure  --with-gnutls --without-gconf --with-rsvg --with-x --with-xwidgets --without-toolkit-scroll-bars --without-xaw3d --without-gsettings --with-mailutils --with-nativecomp CFLAGS="-O3 -mtune=native -march=native -fomit-frame-pointer"
+# (native-compile-async "~/.emacs.d/elpa/" 4 t)
