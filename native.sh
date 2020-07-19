b3a448e44afec0eacf6895bd56e04293eccd4965
Clemens Radermacher <clemera@posteo.net>
2020-07-19 13:49:20 +0200

    Update native.sh

--- native.sh
+++ native.sh
@@ -3,3 +3,4 @@ git checkout feature/native-comp
 ./autogen.sh
 ./configure  --with-gnutls --without-gconf --with-rsvg --with-x --with-xwidgets --without-toolkit-scroll-bars --without-xaw3d --without-gsettings --with-mailutils --with-nativecomp CFLAGS="-O3 -mtune=native -march=native -fomit-frame-pointer"
 # (native-compile-async "~/.emacs.d/elpa/" 4 t)
+# See also https://github.com/mnewt/dotemacs/blob/master/bin/build-gccemacs
